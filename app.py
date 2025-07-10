54f3a0bb5d71b92b90792ceb10ffb5897108653b
jesusinfantes <56434423+jesusinfantes@users.noreply.github.com>
2025-07-10 11:47:46 +0200

    Updated function name

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def about():
     return render_template("about.html")
 
 @app.route("/services")
-def about():
+def services():
     return render_template("services.html")
 
 if __name__ == "__main__":
